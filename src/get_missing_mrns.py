8e42080cd43b9cbeee33329d93c8eadf5a9024c8
Frank Cao <franktcao@gmail.com>
2024-06-20 17:57:50 -0700

    Clean

--- src/get_missing_mrns.py
+++ src/get_missing_mrns.py
@@ -11,40 +11,40 @@ def main(
     result = (
         main_table[[mrn_col]]
         .merge(
-            cgdb[[mrn_col]].dropna().drop_duplicates(), 
-            on=mrn_col, 
-            how="left", 
+            cgdb[[mrn_col]].dropna().drop_duplicates(),
+            on=mrn_col,
+            how="left",
             indicator=True
         )
         .pipe(lambda x: x[x["_merge"] != "both"])
         .filter([mrn_col])
     )
-    
+
     data_path = Path(config["datapath"])
     fname_main = table_names["main"]
     fpath = data_path / fname_main
     fpath = fpath.with_stem(f"missing_mrns")
     result.to_excel(fpath, index=False)
-    
+
     print(f"Result preview:")
     print(result.head())
-    print(f"Result saved to {fpath}")
+    print(f"Result saved to {fpath}!")
 
 
 if __name__ == "__main__":
     config = get_config("missing_mrns.yml")
-    
+
     data_path = Path(config["datapath"])
     table_names = config["tables"]
     mrn_col = config["mrn_col"]
-    
+
     fname_main = table_names["main"]
     fpath = data_path / fname_main
     main_table = pd.read_excel(fpath)
-    
+
     cgdb = table_names["caner_gen_db"]
     fpath = data_path / cgdb
-    
+
     cgdb_table = pd.read_excel(fpath)
     print(cgdb_table)
 
