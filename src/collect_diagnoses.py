df0499bfe59c9c10fd58e31e7231abbc4187d46c
Frank Cao <franktcao@gmail.com>
2024-06-23 19:21:39 -0700

    Add in PCN diagnoses

--- src/collect_diagnoses.py
+++ src/collect_diagnoses.py
@@ -11,6 +11,7 @@ def process_diagnosis(
     columns_1: dict[str, dict], 
     columns_desc: dict[str, dict],
     columns_recheck: dict[str, dict],
+    columns_1_desc_date: dict[str, dict],
 ) -> pd.DataFrame:
     processed = diag_table.copy()
     for out_col, contains in columns_1.items():
@@ -27,6 +28,7 @@ def process_diagnosis(
                 }
             )
         )
+
     for out_col, contains in columns_recheck.items():
         contains_str = "|".join(contains)
         processed = (
@@ -73,18 +75,68 @@ def process_diagnosis(
             )
         )
 
+    for out_col, contains in columns_1_desc_date.items():
+        contains_str = "|".join(contains)
+        processed = (
+            processed
+            .assign(
+                **{
+                    out_col: lambda x: (
+                        x[desc_col]
+                        .str.contains(contains_str, case=False, regex=True)
+                        .astype(int)
+                    )
+                }
+            )
+            .assign(
+                **{
+                    f"{out_col} notes": lambda x: np.where(
+                        x[out_col] == 1, x[desc_col], ""
+                    )
+                }
+            )
+            .assign(
+                **{
+                    f"{out_col} dx date": lambda x: np.where(
+                        x[out_col] == 1, 
+                        x["Date"].astype("datetime64[s]"), 
+                        pd.NaT
+                    )
+                }
+            )
+        )
+
     patient_col = "Patient Id"
     processed = (
         processed
         .groupby(patient_col, as_index=False)
         .agg(
-            {col: "max" for col in columns_1} | 
-            {col: lambda x: " || ".join(set(x)) for col in columns_desc} |
-            {col: "max" for col in columns_recheck} | 
-            {f"{col} notes": lambda x: " || ".join(set(x)) for col in columns_recheck} 
+            {col: "max" for col in columns_1 | columns_recheck} | 
+            {
+                col: lambda x: " || ".join(set([e for e in x if e != ""])) 
+                for col in columns_desc
+            } |
+            {
+                f"{col} notes": lambda x: (
+                    " || ".join(set([e for e in x if e != ""])) 
+                )
+                for col in columns_recheck | columns_1_desc_date
+            } |
+            {f"{col} dx date": "min" for col in columns_1_desc_date} 
         )
         .rename(columns={patient_col: "Patient id"})
-        .filter(["Patient id", *(columns_1.keys()), *(columns_desc.keys()), *(columns_recheck.keys()), *[f"{col} notes" for col in columns_recheck.keys()]])
+        .filter(
+            [
+                "Patient id", 
+                *(columns_1.keys()), 
+                *(columns_desc.keys()), 
+                *(columns_recheck.keys()), 
+                *[f"{col} notes" for col in columns_recheck.keys()]
+                *(columns_1_desc_date.keys()), 
+                *[f"{col} notes" for col in columns_1_desc_date.keys()]
+                *[f"{col} dx date" for col in columns_1_desc_date.keys()]
+            ]
+        )
     )
 
     return processed 
